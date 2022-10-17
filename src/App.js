ec063f52aaf798eb92e62ae38dee2d5447400ec1
HajimeSonkai <mfilho86@gmail.com>
2022-10-17 14:56:57 -0300

    Add: State das categorias

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getcategories } from './Api_controll/ApiConnections';
 
 function App() {
+  const [categories, setcategories] = useState();
+
+  useEffect(() => {
+    console.log("Carregando");
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = async () => {
+    try {
+      const fetch = await getcategories();
+      console.log("getcategoriesresult = ", fetch);
+      const promises = fetch.map(async (result) => {
+        return await result
+      });
+      const results = await Promise.all(promises);
+      setcategories(results)
+      console.log("setcategories = " ,results)
+    } catch (error) {
+      console.log("getcategories Error:", error);
+    }
+  };
+
   return (
     <div className="App">
       <div>Hello World!</div>
+      <div>{categories ? categories.map((categorie, index) => {
+              return <div key={index}>{categorie.name}</div>;
+            }) : "carregando"}</div>
     </div>
   );
 }
