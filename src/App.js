8b46d7be92e64588e931be1b72cc55c984862a9d
HajimeSonkai <mfilho86@gmail.com>
2022-10-17 15:44:47 -0300

    Add: useState de products

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { getcategories } from './Api_controll/ApiConnections';
+import React, { useEffect, useState } from "react";
+import Destaque from "./Components/Destaque";
+import "./App.css";
+import { getcategories, getproducts } from "./Api_controll/ApiConnections";
 
 function App() {
   const [categories, setcategories] = useState();
+  const [products, setproducts] = useState();
 
   useEffect(() => {
     console.log("Carregando");
     fetchCategories();
+    fetchallproducts()
   }, []);
 
   const fetchCategories = async () => {
@@ -15,22 +18,44 @@ function App() {
       const fetch = await getcategories();
       console.log("getcategoriesresult = ", fetch);
       const promises = fetch.map(async (result) => {
-        return await result
+        return await result;
       });
       const results = await Promise.all(promises);
-      setcategories(results)
-      console.log("setcategories = " ,results)
+      setcategories(results);
+      console.log("setcategories = ", results);
     } catch (error) {
       console.log("getcategories Error:", error);
+    }
+  };
+
+  const fetchallproducts = async () => {
+    try {
+      const fetch = await getproducts();
+      console.log("getproductsresult = ", fetch);
+      const promises = fetch.map(async (result) => {
+        return await result;
+      });
+      const results = await Promise.all(promises);
+      setproducts(results);
+      console.log("setproducts = ", results);
+    } catch (error) {
+      console.log("setproducts Error:", error);
     }
   };
 
   return (
     <div className="App">
       <div>Hello World!</div>
-      <div>{categories ? categories.map((categorie, index) => {
+      <div>
+        {categories
+          ? categories.map((categorie, index) => {
               return <div key={index}>{categorie.name}</div>;
-            }) : "carregando"}</div>
+            })
+          : "carregando"}
+      </div>
+      <div className="destaques">
+        {products ? <Destaque products={products}/> : "carregando"}
+      </div>
     </div>
   );
 }
