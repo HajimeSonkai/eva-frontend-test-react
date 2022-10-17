57f8eae7cef295522004fca286e22094c0928d3e
HajimeSonkai <mfilho86@gmail.com>
2022-10-17 15:45:12 -0300

    Add: card de Destaques

---
+++ src/Components/Destaque.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Productcard from "./Productcard";
+
+const Destaque = (props) => {
+  const { products } = props
+    return (
+        <div>
+            {products.map((product, index) => {
+              return <Productcard key={index} product={product} />;
+            })}
+        </div>
+    )
+}
+
+export default Destaque
