9fc22c766fa6b7fe786cc5ead763447db8b818ee
HajimeSonkai <mfilho86@gmail.com>
2022-10-21 17:29:05 -0300

    add: map para transformar informaçãos da api

--- src/Components/Destaquebycat.js
+++ src/Components/Destaquebycat.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Productcard from "./Productcard";
 
 const Destaquebycat = (props) => {
-  const { products, categorie } = props
-    return (
-        <div>
-            <h3>{categorie}</h3>
-            {products.map((product, index) => {
-              return <div key={index} >{product.name}</div>;
-            })}
-        </div>
-    )
-}
+  const { products, categorie, allproducts } = props;
+  const [productsbycatdestac, setproductsbycatdestac] = useState();
 
-export default Destaquebycat
+  useEffect(() => {
+    categorietoproductmap(products, allproducts);
+  }, [products, allproducts]);
+
+  const categorietoproductmap = (products, allproducts) => {
+    const mainprodcs = [];
+
+    products.map((prod) => {
+      return allproducts.map((allprod) => {
+        if (allprod.id === prod.id) {
+          mainprodcs.push(allprod);
+        }
+        return mainprodcs;
+      });
+    });
+
+    setproductsbycatdestac(mainprodcs);
+  };
+
+  return (
+    <div>
+      <h3>{categorie}</h3>
+      {productsbycatdestac.map((product, index) => {
+        return <Productcard key={index} product={product} />;
+      })}
+    </div>
+  );
+};
+
+export default Destaquebycat;
